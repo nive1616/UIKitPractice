6bf04ead95cf7351b5a163514997da3bdd689c39
nive1616 <nchellam3@gatech.edu>
2022-02-16 15:19:30 -0500

    Initial Commit

---
+++ UIKitPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitPractice
+//
+//  Created by Nivedita Chellam on 2/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
